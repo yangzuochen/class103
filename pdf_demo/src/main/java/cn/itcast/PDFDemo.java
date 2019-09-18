054aa64ed67e8d84c6d97cbea05cb9b9cd58b456
syl <yangzuochen@126.com>
2019-09-18 20:42:53 +0800

    李四的方法

--- pdf_demo/src/main/java/cn/itcast/PDFDemo.java
+++ pdf_demo/src/main/java/cn/itcast/PDFDemo.java
@@ -71,7 +71,17 @@ public class PDFDemo {
 
     }
 
+ public static Connection lisi() {
+        try {
+            Class.forName ( "com.mysql.jdbc.Driver" );
+            Connection connection = DriverManager.getConnection ( "jdbc:mysql:///saas-export-103", "root", "root" );
+            return connection;
+        } catch (Exception e) {
+            e.printStackTrace ();
+            return null;
+        }
 
+    }
 
 
 
